8ed7477724b688e1ad34fbc5a4da001b95e24f08
Arun <121216673+arunghalli0502@users.noreply.github.com>
2023-11-11 19:01:59 +0530

    Update Arraysortsdes.js

--- Arraysortsdes.js
+++ Arraysortsdes.js
@@ -1,8 +1,18 @@
-function Arraysortdes() 
-{
-let fruites= ["jackfruits","apple", "organge", "banana", "mango", "lime"];
-let res= fruites.sort();
-console.log(res);
+function Arraysortdes() {
+    let fruites = ["jackfruits", "apple", "organge", "banana", "mango", "lime"];
+    let res = fruites.sort();
+    console.log(res);
+    // console.log(fruites);
+    for (let i = 0; i < fruites.length; i++) {
+       
+        {
+            if (fruites[i] > fruites[i+1]) 
+            {
+                [fruites[i], fruites[i+ 1]] = [fruites[i+ 1], fruites[i]];
+            } 
+        }
+    }
+   console.log(fruites);
 }
 
 Arraysortdes();
